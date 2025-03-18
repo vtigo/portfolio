96a3930d635cdcaedaa01b9d0093e5ea8a7fb765
Tigo <rf.tigas@gmail.com>
2025-03-18 14:39:46 -0300

    chore: add depolyment files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://vtigo.github.io",
+  base: "/portfolio",
 });
